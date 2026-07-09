8e43a174d6d8089ebce12006993a9ada74882231
Hubert Boryś <hubert.borys23@gmail.com>
2026-07-09 22:51:28 +0200

    Initial iris-core project

---
+++ src/main/java/com/iris/core/IrisCore.java
@@ -0,0 +1,13 @@
+package com.iris.core;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(IrisCore.MOD_ID)
+public class IrisCore {
+
+    public static final String MOD_ID = "iriscore";
+
+    public IrisCore() {
+        System.out.println("[Iris Core] Loaded successfully.");
+    }
+}
